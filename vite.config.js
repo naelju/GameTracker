b775c4a231f041a8b2b7290f7d19409a3c5b9052
Naël Juston <nael.juston@chiliz.com>
2025-09-18 20:43:17 +0200

    Remove all deployment-related files and dependencies

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/GameTracker/', // Use relative paths for GitHub Pages
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js'
-      }
-    }
-  }
+  plugins: [react()]
 })
