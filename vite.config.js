4f0a9ce9d29ce16a7d0b3c9d9d28a79c9e242e04
Naël Juston <nael.juston@chiliz.com>
2025-09-18 20:30:23 +0200

    Change deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use relative paths for GitHub Pages
+  base: '/', // Use relative paths for GitHub Pages
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
